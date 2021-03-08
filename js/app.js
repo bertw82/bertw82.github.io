7ae3e3d18d4f8f3304b9263600a1dea755411b1f
Bert Witzel <bwitzel82@gmail.com>
2021-03-08 14:04:01 -0500

    tidy up and fix form submission

--- js/app.js
+++ js/app.js
@@ -5,20 +5,29 @@ $(function () {
 
 // form submission
 const button = document.getElementById('form-button');
-button.addEventListener('click', () => {
-    const name = document.getElementById('exampleFormControlInput1');
-    const email = document.getElementById('exampleFormControlInput2');
-
+button.addEventListener('click', (e) => {
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const phone = document.getElementById('phone');
+    const comments = document.getElementById('comments');
+    e.preventDefault();
     if (name.value === '' && email.value === '') {
-        return alert('Please enter your name and email address');
+        alert('Please enter your name and email address');
+        e.stopPropagation();
     } else if (name.value === '') {
-        return alert('Please enter your name');
+        alert('Please enter your name');
+        e.stopPropagation();
     } else if (email.value === '') {
-        return alert('Please enter your email address');
+        alert('Please enter your email address');
+        e.stopPropagation();
     } else {
         $('#myModal').on('shown.bs.modal', function () {
             $('#myInput').trigger('focus')
             });
+        name.value = '';
+        email.value = '';
+        phone.value = '';
+        comments.value = '';
     }
 });
 
