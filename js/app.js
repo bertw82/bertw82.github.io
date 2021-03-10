6ce3a91efaa43dc79662c2272644d62601fe3b18
Bert Witzel <bwitzel82@gmail.com>
2021-03-09 19:48:19 -0500

    work on dark theme js

--- js/app.js
+++ js/app.js
@@ -43,6 +43,10 @@ darkThemeButton.addEventListener('click', () => {
     const nav = document.getElementById('nav');
     nav.classList.add('navbar-dark', 'bg-dark', 'border-secondary');
     nav.classList.remove('navbar-light', 'bg-light', 'border-muted');
+    // change jumbotron styles
+    const jumbotron = document.querySelector('.jumbotron');
+    jumbotron.classList.add('bg-success');
+    jumbotron.classList.remove('bg-info');
     // change footer nav styles
     const footerNav = document.getElementById('footerNav');
     footerNav.classList.add('bg-dark', 'border-dark');
@@ -50,6 +54,50 @@ darkThemeButton.addEventListener('click', () => {
     const footerText = document.querySelector('small');
     footerText.parentNode.classList.add('text-light');
     footerText.parentNode.classList.remove('text-dark');
+    // change h2 text color
+    const h2 = document.getElementsByTagName('h2');
+    for (let i = 0; i < h2.length; i++) {
+        h2[i].classList.add('text-light');
+        h2[i].classList.remove('text-muted');
+    };
+    // change lead paragraph styles
+    const leadParagraph = document.querySelector('.p-about');
+    leadParagraph.classList.add('text-light');
+    leadParagraph.classList.remove('text-body');
+    const leadLink = document.querySelector('.text-primary');
+    leadLink.classList.add('text-success');
+    leadLink.classList.remove('text-primary');
+    // change card styles
+    const card = document.querySelectorAll('.h-100');
+    for (let i = 0; i < card.length; i++) {
+        card[i].classList.add('bg-dark', 'border-primary');
+        card[i].classList.remove('bg-light', 'border-muted');
+    }
+    const cardBody = document.querySelectorAll('.card-body');
+    for (let i = 0; i < cardBody.length; i++) {
+        cardBody[i].classList.add('border-primary');
+        cardBody[i].classList.remove('border-muted');
+    }
+    const cardTitle = document.querySelectorAll('.card-title');
+    for (let i= 0; i < cardTitle.length; i++) {
+        cardTitle[i].classList.add('text-light');
+        // cardTitle[i].classList.remove('text-body');
+    }
+    const cardText = document.querySelectorAll('.card-text');
+    for (let i= 0; i < cardText.length; i++) {
+        cardText[i].classList.add('text-light');
+        // cardTitle[i].classList.remove('text-body');
+    }
+    const btnProject = document.querySelectorAll('.btn-project');
+    for (let i = 0; i < btnProject.length; i++) {
+        btnProject[i].classList.add('btn-danger');
+        btnProject[i].classList.remove('btn-primary');
+    }
+    const btnCode = document.querySelectorAll('.btn-code');
+    for (let i = 0; i < btnCode.length; i++) {
+        btnCode[i].classList.add('btn-info');
+        btnCode[i].classList.remove('btn-success');
+    }
 });
 
 
