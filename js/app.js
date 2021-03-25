89a5c8280075ca709e7754821f601eaed3e22e37
Bert Witzel <bwitzel82@gmail.com>
2021-03-25 19:24:08 -0400

    experiment w form

--- js/app.js
+++ js/app.js
@@ -23,32 +23,32 @@ function getDate() {
 getDate();
 
 // form submission functionality
-const button = document.getElementById('form-button');
-button.addEventListener('click', (e) => {
-    const name = document.getElementById('name');
-    const email = document.getElementById('email');
-    const phone = document.getElementById('phone');
-    const comments = document.getElementById('comments');
-    e.preventDefault();
-    if (name.value === '' && email.value === '') {
-        alert('Please enter your name and email address');
-        e.stopPropagation();
-    } else if (name.value === '') {
-        alert('Please enter your name');
-        e.stopPropagation();
-    } else if (email.value === '') {
-        alert('Please enter your email address');
-        e.stopPropagation();
-    } else {
-        $('#myModal').on('shown.bs.modal', function () {
-            $('#myInput').trigger('focus')
-            });
-        name.value = '';
-        email.value = '';
-        phone.value = '';
-        comments.value = '';
-    }
-});
+// const button = document.getElementById('form-button');
+// button.addEventListener('click', (e) => {
+//     const name = document.getElementById('name');
+//     const email = document.getElementById('email');
+//     const phone = document.getElementById('phone');
+//     const comments = document.getElementById('comments');
+//     e.preventDefault();
+//     if (name.value === '' && email.value === '') {
+//         alert('Please enter your name and email address');
+//         e.stopPropagation();
+//     } else if (name.value === '') {
+//         alert('Please enter your name');
+//         e.stopPropagation();
+//     } else if (email.value === '') {
+//         alert('Please enter your email address');
+//         e.stopPropagation();
+//     } else {
+//         $('#myModal').on('shown.bs.modal', function () {
+//             $('#myInput').trigger('focus')
+//             });
+//         name.value = '';
+//         email.value = '';
+//         phone.value = '';
+//         comments.value = '';
+//     }
+// });
 
 // dark theme JS
 const darkThemeButton = document.getElementById('darkTheme');
