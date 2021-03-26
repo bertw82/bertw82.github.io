ec39b13e9264cfe0023d284016f063a6eee6ce8d
Bert Witzel <bwitzel82@gmail.com>
2021-03-26 09:48:05 -0400

    refractor code, change quote styles

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-// enable Bootstrap popover
+/*** 
+ * enable bootstrap popover on Bootstrap icon
+ ***/ 
+
 $(function () {
     $('[data-toggle="popover"]').popover()
   });
@@ -7,7 +10,11 @@ $('.popover-dismiss').popover({
     trigger: 'focus'
   });
 
-//   initialize toast
+/*** 
+ * jquery to initialize 'toast'
+ * function to get current date to display on 'toast'
+ ***/ 
+
 $('.toast').toast('show');
 
 const toastDate = document.querySelector('.mr-auto');
@@ -22,177 +29,168 @@ function getDate() {
 }
 getDate();
 
-// dark theme JS
-const darkTheme = document.getElementById('darkTheme');
+/*** 
+ * Variables for Light and Dark Themes 
+ ***/ 
 
+const body = document.querySelector('body');
+const nav = document.getElementById('nav');
+const footerNav = document.getElementById('footerNav');
+const footerText = document.querySelector('small');
+const h2 = document.getElementsByTagName('h2');
+const leadParagraph = document.querySelector('.p-about');
+const card = document.querySelectorAll('.h-100');
+const cardBody = document.querySelectorAll('.card-body');
+const cardTitle = document.querySelectorAll('.card-title');
+const cardText = document.querySelectorAll('.card-text');
+const btnProject = document.querySelectorAll('.btn-project');
+const btnCode = document.querySelectorAll('.btn-code');
+const form = document.querySelector('form');
+const label = document.querySelectorAll('label');
+const formButton = document.getElementById('form-button');
+const quote = document.querySelector('blockquote');
+const quoteName = document.querySelector('.blockquote-footer');
+const quoteBody = document.querySelector('.quote-body');
+
+/***
+ * Dark Theme function
+ * clicking 'Dark' button changes classes to a Dark Theme 
+ ***/ 
+
+const darkTheme = document.getElementById('darkTheme');
 darkTheme.addEventListener('click', () => {
     // change body color
-    const body = document.querySelector('body');
     body.classList.add('body-background');
     body.classList.remove('bg-white');
     // change nav styles
-    const nav = document.getElementById('nav');
     nav.classList.add('navbar-dark', 'bg-dark', 'border-secondary');
     nav.classList.remove('navbar-light', 'bg-light', 'border-muted');
     // change footer nav styles
-    const footerNav = document.getElementById('footerNav');
     footerNav.classList.add('bg-dark', 'border-secondary');
     footerNav.classList.remove('bg-light', 'border-muted');
-    const footerText = document.querySelector('small');
     footerText.parentNode.classList.add('text-light');
     footerText.parentNode.classList.remove('text-dark');
     // change h2 text color
-    const h2 = document.getElementsByTagName('h2');
     for (let i = 0; i < h2.length; i++) {
         h2[i].classList.add('text-light');
         h2[i].classList.remove('text-muted');
     }
     // change lead paragraph styles
-    const leadParagraph = document.querySelector('.p-about');
     leadParagraph.classList.add('text-light');
     leadParagraph.classList.remove('text-body');
     const leadLink = document.querySelector('.text-primary');
     leadLink.classList.add('text-success');
     leadLink.classList.remove('text-primary');
     // change card styles
-    const card = document.querySelectorAll('.h-100');
     for (let i = 0; i < card.length; i++) {
         card[i].classList.add('bg-dark', 'border-primary');
         card[i].classList.remove('bg-light', 'border-muted');
     }
-    const cardBody = document.querySelectorAll('.card-body');
     for (let i = 0; i < cardBody.length; i++) {
         cardBody[i].classList.add('border-primary');
         cardBody[i].classList.remove('border-muted');
     }
-    const cardTitle = document.querySelectorAll('.card-title');
     for (let i= 0; i < cardTitle.length; i++) {
         cardTitle[i].classList.add('text-light');
-        // cardTitle[i].classList.remove('text-body');
     }
-    const cardText = document.querySelectorAll('.card-text');
     for (let i= 0; i < cardText.length; i++) {
         cardText[i].classList.add('text-light');
-        // cardTitle[i].classList.remove('text-body');
     }
     // button styles
-    const btnProject = document.querySelectorAll('.btn-project');
     for (let i = 0; i < btnProject.length; i++) {
         btnProject[i].classList.add('btn-danger');
         btnProject[i].classList.remove('btn-primary');
     }
-    const btnCode = document.querySelectorAll('.btn-code');
     for (let i = 0; i < btnCode.length; i++) {
         btnCode[i].classList.add('btn-info');
         btnCode[i].classList.remove('btn-success');
     }
     // form styles
-    const form = document.querySelector('form');
     form.classList.add('bg-secondary', 'border-primary');
     form.classList.remove('bg-light', 'border-info');
-    const label = document.querySelectorAll('label');
     for (let i = 0; i < label.length; i++) {
         label[i].classList.add('text-light');
         label[i].classList.remove('text-body');
     }
-    const formButton = document.getElementById('form-button');
     formButton.classList.add('btn-primary');
     formButton.classList.remove('btn-info');
     // quote styles
-    const quote = document.querySelector('blockquote');
     quote.classList.add('text-warning');
     quote.classList.remove('text-dark');
-
-    const quoteName = document.querySelector('.blockquote-footer');
     quoteName.classList.add('text-warning');
     quoteName.classList.remove('text-muted');
+    quoteBody.classList.add('bg-dark');
 });
 
-// light theme JS
-const lightTheme = document.getElementById('lightTheme');
+/*** 
+ * Light Theme function
+ * clicking the 'Light' button changes the classes back
+ ***/
 
+const lightTheme = document.getElementById('lightTheme');
 lightTheme.addEventListener('click', () => {
      // change body color
-     const body = document.querySelector('body');
      body.classList.remove('body-background');
      body.classList.add('bg-white');
      // change nav styles
-     const nav = document.getElementById('nav');
      nav.classList.remove('navbar-dark', 'bg-dark', 'border-secondary');
      nav.classList.add('navbar-light', 'bg-light', 'border-muted');
      // change footer nav styles
-     const footerNav = document.getElementById('footerNav');
      footerNav.classList.remove('bg-dark', 'border-secondary');
      footerNav.classList.add('bg-light', 'border-muted');
-     const footerText = document.querySelector('small');
      footerText.parentNode.classList.remove('text-light');
      footerText.parentNode.classList.add('text-dark');
      // change h2 text color
-     const h2 = document.getElementsByTagName('h2');
      for (let i = 0; i < h2.length; i++) {
          h2[i].classList.remove('text-light');
          h2[i].classList.add('text-muted');
      }
      // change lead paragraph styles
-     const leadParagraph = document.querySelector('.p-about');
      leadParagraph.classList.remove('text-light');
      leadParagraph.classList.add('text-body');
      const leadLink = document.querySelector('a[href="https://teamtreehouse.com"]');
      leadLink.classList.add('text-primary');
      leadLink.classList.remove('text-success');
      // change card styles
-     const card = document.querySelectorAll('.h-100');
      for (let i = 0; i < card.length; i++) {
          card[i].classList.remove('bg-dark', 'border-primary');
          card[i].classList.add('bg-light', 'border-muted');
      }
-     const cardBody = document.querySelectorAll('.card-body');
      for (let i = 0; i < cardBody.length; i++) {
          cardBody[i].classList.remove('border-primary');
          cardBody[i].classList.add('border-muted');
      }
-     const cardTitle = document.querySelectorAll('.card-title');
      for (let i= 0; i < cardTitle.length; i++) {
          cardTitle[i].classList.remove('text-light');
-        //  cardTitle[i].classList.add('text-body');
      }
-     const cardText = document.querySelectorAll('.card-text');
      for (let i= 0; i < cardText.length; i++) {
          cardText[i].classList.remove('text-light');
-         // cardTitle[i].classList.remove('text-body');
      }
      // button styles
-     const btnProject = document.querySelectorAll('.btn-project');
      for (let i = 0; i < btnProject.length; i++) {
          btnProject[i].classList.remove('btn-danger');
          btnProject[i].classList.add('btn-primary');
      }
-     const btnCode = document.querySelectorAll('.btn-code');
      for (let i = 0; i < btnCode.length; i++) {
          btnCode[i].classList.remove('btn-info');
          btnCode[i].classList.add('btn-success');
      }
      // form styles
-     const form = document.querySelector('form');
      form.classList.remove('bg-secondary', 'border-primary');
      form.classList.add('bg-light', 'border-info');
-     const label = document.querySelectorAll('label');
     for (let i = 0; i < label.length; i++) {
         label[i].classList.remove('text-light');
         label[i].classList.add('text-body');
     }
-     const formButton = document.getElementById('form-button');
      formButton.classList.remove('btn-primary');
      formButton.classList.add('btn-info');
      // quote styles
-     const quote = document.querySelector('blockquote');
      quote.classList.remove('text-warning');
      quote.classList.add('text-dark');
- 
-     const quoteName = document.querySelector('.blockquote-footer');
      quoteName.classList.remove('text-warning');
      quoteName.classList.add('text-muted');
-
+     quoteBody.classList.remove('bg-dark');
+     // address buggy behavior with radio buttons 
      lightTheme.parentNode.classList.remove('text-body');
      lightTheme.parentNode.classList.add('text-light');
      darkTheme.parentNode.classList.remove('text-body');
