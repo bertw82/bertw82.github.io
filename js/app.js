7b2fe70c6c3461ddd7dd41110dbb27d7ee134afe
Bert Witzel <bwitzel82@gmail.com>
2021-03-29 22:50:23 -0400

    Update html, css, js
    add translate js feature for section titles, adjust background colors, fix navbar issue in mobile

--- js/app.js
+++ js/app.js
@@ -10,24 +10,46 @@ $('.popover-dismiss').popover({
     trigger: 'focus'
   });
 
+// const toastDate = document.querySelector('.mr-auto');
+// function getDate() {
+//     const today = new Date();
+//     const day = today.getDate();
+//     const month = today.getMonth();
+//     const year = today.getFullYear();
+//     const monthsLong = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+//     const monthName = monthsLong[month];
+//     toastDate.textContent = `${monthName} ${day}, ${year}`;
+// }
+// getDate();
+
 /*** 
- * jquery to initialize 'toast'
- * function to get current date to display on 'toast'
+ * Section title translate
  ***/ 
 
-$('.toast').toast('show');
+// select all boxes
+const translate = document.querySelectorAll('.translate')
+// listen for scroll event and call the checkBoxes function every time the user scrolls 
+window.addEventListener('scroll', checkBoxes)
+// call function on pageload so the first boxes are displayed
+checkBoxes()
 
-const toastDate = document.querySelector('.mr-auto');
-function getDate() {
-    const today = new Date();
-    const day = today.getDate();
-    const month = today.getMonth();
-    const year = today.getFullYear();
-    const monthsLong = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const monthName = monthsLong[month];
-    toastDate.textContent = `${monthName} ${day}, ${year}`;
+
+function checkBoxes() {
+  // variable stores the point at which you want the element to move into the page. In this example at 80% from the top.
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    translate.forEach(box => {
+      // check the location of the box on the page
+        const boxTop = box.getBoundingClientRect().top
+
+        // show the box when its further down the page then the 80% from the top
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
 }
-getDate();
 
 /*** 
  * Variables for Light and Dark Themes 
@@ -50,7 +72,7 @@ const label = document.querySelectorAll('label');
 const formButton = document.getElementById('form-button');
 const quote = document.querySelector('blockquote');
 const quoteName = document.querySelector('.blockquote-footer');
-const quoteBody = document.querySelector('.quote-body');
+const quoteBody = document.querySelector('.js-quote');
 
 /***
  * Dark Theme function
@@ -76,8 +98,8 @@ darkTheme.addEventListener('click', () => {
         h2[i].classList.remove('text-muted');
     }
     // change lead paragraph styles
-    leadParagraph.classList.add('text-light');
-    leadParagraph.classList.remove('text-body');
+    leadParagraph.classList.add('text-light', 'bg-dark');
+    leadParagraph.classList.remove('text-body', 'background-alt');
     const leadLink = document.querySelector('.text-primary');
     leadLink.classList.add('text-success');
     leadLink.classList.remove('text-primary');
@@ -146,8 +168,8 @@ lightTheme.addEventListener('click', () => {
          h2[i].classList.add('text-muted');
      }
      // change lead paragraph styles
-     leadParagraph.classList.remove('text-light');
-     leadParagraph.classList.add('text-body');
+     leadParagraph.classList.remove('text-light', 'bg-dark');
+     leadParagraph.classList.add('text-body', 'background-alt');
      const leadLink = document.querySelector('a[href="https://teamtreehouse.com"]');
      leadLink.classList.add('text-primary');
      leadLink.classList.remove('text-success');
@@ -190,6 +212,7 @@ lightTheme.addEventListener('click', () => {
      quoteName.classList.remove('text-warning');
      quoteName.classList.add('text-muted');
      quoteBody.classList.remove('bg-dark');
+     quoteBody.classList.add('background-alt');
      // address buggy behavior with radio buttons 
      lightTheme.parentNode.classList.remove('text-body');
      lightTheme.parentNode.classList.add('text-light');
